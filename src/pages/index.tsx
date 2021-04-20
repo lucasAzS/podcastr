8af839b9b81046ee4d253da755e0955130eea8fa
Lucas Azambuja Santos <lucasazsantos@gmail.com>
2021-04-20 20:44:35 -0300

    player and static generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-import { Header } from '../components/Header';
+export default function Home({ episodes }) {
+  console.log(episodes);
 
-export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
